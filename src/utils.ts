0a998ff9c0a1c84fea8f8b9f1b0fd248fda6df3e
ellery0924 <383188743@qq.com>
2017-04-23 20:25:25 +0800

    initial

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ export function randomize(min: number, max: number): number {
     return parseInt(String(min + Math.random() * (max - min)), 10);
 }
 
-export function getRandomRects() {
+export function getRandomRects(): Array<{ width: number, height: number, other: number }> {
     return new Array(10).fill(1).map((_, i) => ({
         width: randomize(300, 400),
         height: randomize(300, 400),
